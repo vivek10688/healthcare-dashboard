46883c41c4ebb2478e9b5be2d4f760d461ab61b4
Vivek Singh <er.vivek.it@gmail.com>
2025-11-08 13:34:43 +0530

    Fix: app layout fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,12 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if (app()->environment('testing'))
+    <!-- Skip vite in tests -->
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+
 </head>
 <body class="font-sans antialiased bg-gray-100 text-gray-900">
     <div class="min-h-screen bg-gray-100">
